af82e8b2142436c641c45a1e213c6756c7d2990f
Vanille-N <neven.villani@gmail.com>
2020-12-30 09:30:50 +0100

    wifi widgets

---
+++ scripts/network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kitty --class Scanner --config ~/.config/kitty/kitty.conf \
+    --override initial_window_width=90c \
+    -e ~/.config/scripts/nm.sh &
+cpid=$!
+sleep 0.05
+nm-connection-editor
+kill $cpid
