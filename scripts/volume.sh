b3ea20e2d9c28119dabf03783409f6a5a66c69fc
Vanille-N <neven.villani@gmail.com>
2021-01-01 08:13:49 +0100

    fmt volume.sh

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
 kitty --class Visualizer \
-    --override font_size=2.0 \
-    --override background="#101010" \
+    --config ~/.config/kitty/kitty.conf \
     --override initial_window_width=190c \
     --override initial_window_height=80c \
+    --override background="#101010" \
+    --override font_size=2.0 \
     -e vis &
 cpid=$!
 sleep 0.1
 kitty --class Mixer \
+    --config ~/.config/kitty/kitty.conf \
     --override initial_window_width=80c \
     --override initial_window_height=43c \
     -e alsamixer
