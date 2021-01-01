0d3d55b0d8ad74cfa3fdf353a9db0d83e3155704
Vanille-N <neven.villani@gmail.com>
2021-01-01 08:13:36 +0100

    fmt resources.sh

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-kitty --class Bottom --config ~/.config/kitty/kitty.conf \
+kitty --class Bottom \
+    --config ~/.config/kitty/kitty.conf \
     --override initial_window_width=93c \
-    --override font_size=7.0 \
     --override initial_window_height=60c \
+    --override font_size=7.0 \
     -e btm &
 cpid=$!
 sleep 0.05
-kitty --class Top --config ~/.config/kitty/kitty.conf \
+kitty --class Top \
+    --config ~/.config/kitty/kitty.conf \
     -e htop
 kill $cpid
