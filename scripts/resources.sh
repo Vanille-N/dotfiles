856627d4e389de3f51cbe65b1e31996913eccccf
Vanille-N <neven.villani@gmail.com>
2021-01-17 13:51:36 +0100

    resources panel tweaks

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+wsprev="$( ~/.config/scripts/i3-curr-ws )"
+
+pkill htop && exit 1
+
+i3 workspace "100:×"
 kitty --class Bottom \
     --config ~/.config/kitty/kitty.conf \
-    --override initial_window_width=93c \
-    --override initial_window_height=60c \
     --override font_size=7.0 \
     -e btm &
+    # --override initial_window_width=93c \
+    # --override initial_window_height=60c \
 cpid=$!
 sleep 0.05
 kitty --class Top \
     --config ~/.config/kitty/kitty.conf \
     -e htop
 kill $cpid
+
+newws="$( ~/.config/scripts/i3-curr-ws )"
+if [[ "$newws" = "100:×" ]]; then
+    i3 workspace "$wsprev"
+fi
