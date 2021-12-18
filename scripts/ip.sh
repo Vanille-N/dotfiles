40f3d95d0d386c8fe04e6ae632bb24e115530dae
Vanille-N <neven.villani@crans.org>
2021-12-18 12:08:30 +0100

    updated some scripts

--- scripts/ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while sleep 1; do
-    data="$( ip -c a )"
-    clear
-    echo -e "$data"
-done
--- scripts/nm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-figlet "Scanning network..."
-
-while sleep 1; do
-    data="$( nmcli --pretty --color yes dev wifi )"
-    clear
-    echo -e "$data"
-done
